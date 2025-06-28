c3fc0b0a9ea9deeba31f7bf3e875167b35bb9162
Abdelrahim Khader <abed4323@outlook.com>
2025-06-28 23:10:13 +0300

    Make app Render-compatible

--- app.py
+++ app.py
@@ -507,4 +507,8 @@ def cellular_capacity():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(
+        debug=False,  # should be False in production
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000))  # Render sets PORT automatically
+    )
